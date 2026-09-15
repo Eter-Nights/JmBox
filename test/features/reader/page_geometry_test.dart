Eter-Nights/jmcomic

feat(reader): 实现章节阅读、预取与状态控制

test/features/reader/page_geometry_test.dart
@@ -1,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmcomic/core/constants/reader_config.dart';
+import 'package:jmcomic/features/reader/page_geometry.dart';
+
+void main() {
+  test('未测页面使用默认比例', () {
+    final geometry = PageGeometry();
+    expect(geometry.aspectOf(0), kDefaultPageAspect);
+    expect(geometry.aspectOf(-1), kDefaultPageAspect);
+  });
+
+  test('本页实测值优先于章内估算', () {
+    final geometry = PageGeometry();
+    geometry.record(0, 1000, 1500);
+    geometry.record(1, 1000, 1700);
+    expect(geometry.aspectOf(0), closeTo(1.5, 1e-9));
+    expect(geometry.aspectOf(9), closeTo(1.7, 1e-9));
+  });
+
+  test('非法页码或尺寸不会污染估算', () {
+    final geometry = PageGeometry();
+    geometry.record(-1, 100, 200);
+    geometry.record(0, 0, 200);
+    geometry.record(1, 100, 0);
+    expect(geometry.aspectOf(5), kDefaultPageAspect);
+  });
+
+  test('异常比例被夹在安全上下限', () {
+    final geometry = PageGeometry();
+    geometry.record(0, 1000, 1);
+    geometry.record(1, 1, 100000);
+    expect(geometry.aspectOf(0), kMinPageAspect);
+    expect(geometry.aspectOf(1), kMaxPageAspect);
+  });
+
+  test('同一页面重新测量后，中位数只统计该页最新值', () {
+    final geometry = PageGeometry();
+    geometry.record(0, 1000, 1000);
+    geometry.record(1, 1000, 3000);
+    geometry.record(0, 1000, 2000);
+
+    // 当前有效页面值为 [2.0, 3.0]，上中位数应为 3.0；旧的 1.0 不应残留为额外样本。
+    expect(geometry.aspectOf(99), closeTo(3.0, 1e-9));
+  });
+
+  test('样本超过上限仍保留每页精确实测值', () {
+    final geometry = PageGeometry();
+    for (var i = 0; i < kGeometrySampleLimit + 5; i++) {
+      geometry.record(i, 1000, 1000 + i);
+    }
+    expect(geometry.aspectOf(0), closeTo(1.0, 1e-9));
+    expect(geometry.aspectOf(kGeometrySampleLimit + 4), closeTo(1.02, 1e-9));
+  });
+}

test/features/reader/reader_controller_test.dart
@@ -1,0 +1,236 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmcomic/data/models/album.dart';
+import 'package:jmcomic/features/reader/reader_controller.dart';
+
+List<Series> _catalog() => const [
+  Series(id: 100, name: '第一话', sort: '1'),
+  Series(id: 101, name: '第二话', sort: '2'),
+  Series(id: 102, name: '第三话', sort: '3'),
+];
+
+class _FakeReaderDataSource implements ReaderDataSource {
+  _FakeReaderDataSource(this.directory, {List<Series>? catalog}) : catalog = catalog ?? _catalog();
+
+  final Directory directory;
+  final List<Series> catalog;
+  final Map<int, int> chapterFetches = {};
+  final Map<int, int> scrambleFetches = {};
+  final List<String> photoReads = [];
+  final List<String> savedProgress = [];
+  final Set<int> failingChapters = {};
+  final Set<int> failingScrambles = {};
+  final Set<String> failPhotoOnce = {};
+
+  @override
+  Future<Chapter> fetchChapter(int chapterId) async {
+    chapterFetches.update(chapterId, (count) => count + 1, ifAbsent: () => 1);
+    if (failingChapters.contains(chapterId)) throw StateError('chapter $chapterId failed');
+    return Chapter(
+      id: chapterId,
+      series: catalog,
+      images: [for (var i = 0; i < 10; i++) '$i.webp'],
+    );
+  }
+
+  @override
+  Future<int> fetchScrambleId(int chapterId) async {
+    scrambleFetches.update(chapterId, (count) => count + 1, ifAbsent: () => 1);
+    if (failingScrambles.contains(chapterId)) {
+      throw StateError('scramble $chapterId failed');
+    }
+    return 220980 + chapterId;
+  }
+
+  @override
+  Future<File> readPhoto(int chapterId, String imageName) async {
+    final key = '$chapterId/$imageName';
+    photoReads.add(key);
+    if (failPhotoOnce.remove(key)) throw StateError('photo failed');
+    return File('${directory.path}/photo.bin');
+  }
+
+  @override
+  Future<void> saveProgress(int albumId, int chapterId) async {
+    savedProgress.add('$albumId/$chapterId');
+  }
+}
+
+Iterable<({int index, double leading, double trailing})> _at(int index) => [
+  (index: index, leading: 0.0, trailing: 1.0),
+];
+
+void main() {
+  late Directory tempDir;
+  late _FakeReaderDataSource dataSource;
+
+  setUp(() async {
+    tempDir = await Directory.systemTemp.createTemp('jmcomic_reader_test_');
+    await File('${tempDir.path}/photo.bin').writeAsBytes([101, 6]);
+    dataSource = _FakeReaderDataSource(tempDir);
+  });
+
+  tearDown(() async {
+    if (await tempDir.exists()) await tempDir.delete(recursive: true);
+  });
+
+  ReaderController createController({int initialChapterId = 101, int? albumId = 9}) {
+    final controller = ReaderController(
+      dataSource: dataSource,
+      albumId: albumId,
+      initialChapterId: initialChapterId,
+    );
+    addTearDown(controller.dispose);
+    return controller;
+  }
+
+  test('初始章加载后建立章节列表、预热相邻章并保存进度', () async {
+    final controller = createController();
+    expect(controller.isLoading, isTrue);
+
+    await pumpEventQueue();
+
+    expect(controller.currentChapter?.id, 101);
+    expect(controller.chapterIndex, 1);
+    expect(controller.chapter.name, '第二话');
+    expect(controller.pageCount, 10);
+    expect(dataSource.chapterFetches.keys, containsAll([100, 101, 102]));
+    expect(dataSource.chapterFetches[101], 1);
+    expect(dataSource.savedProgress, ['9/101']);
+  });
+
+  test('albumId 缺失时不保存进度', () async {
+    createController(albumId: null);
+    await pumpEventQueue();
+    expect(dataSource.savedProgress, isEmpty);
+  });
+
+  test('可视面积最大的页面成为当前页并触发有序预取', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    controller.updateFromPositions([
+      (index: 2, leading: -0.8, trailing: 0.2),
+      (index: 3, leading: 0.2, trailing: 0.9),
+    ]);
+    await pumpEventQueue();
+
+    expect(controller.currentPage, 3);
+    expect(dataSource.photoReads, [
+      '101/4.webp',
+      '101/5.webp',
+      '101/6.webp',
+      '101/7.webp',
+      '101/8.webp',
+      '101/2.webp',
+      '101/1.webp',
+    ]);
+  });
+
+  test('重复覆盖同一预取窗口不会重复读取，失败页允许重试', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    dataSource.failPhotoOnce.add('101/4.webp');
+    controller.updateFromPositions(_at(3));
+    await pumpEventQueue();
+    controller.updateFromPositions(_at(5));
+    await pumpEventQueue();
+
+    expect(dataSource.photoReads.where((key) => key == '101/4.webp'), hasLength(2));
+    expect(dataSource.photoReads.where((key) => key == '101/6.webp'), hasLength(1));
+  });
+
+  test('跳页夹取边界，Slider 松手才真正跳页', () async {
+    final scrolled = <int>[];
+    final controller = ReaderController(
+      dataSource: dataSource,
+      albumId: 9,
+      initialChapterId: 101,
+      requestScroll: scrolled.add,
+    );
+    addTearDown(controller.dispose);
+    await pumpEventQueue();
+
+    controller.jumpToPage(99);
+    expect(controller.currentPage, 9);
+    expect(scrolled, [9]);
+    controller.onSliderChanged(3.6);
+    expect(controller.currentPage, 9);
+    expect(controller.sliderDraft, 3.6);
+    controller.onSliderEnd();
+    expect(controller.currentPage, 4);
+    expect(controller.sliderDraft, isNull);
+    expect(scrolled, [9, 4]);
+  });
+
+  test('切章重置页面草稿、复用预热缓存并保存新进度', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    controller.jumpToPage(8);
+    controller.onSliderChanged(4);
+    controller.selectNextChapter();
+    await pumpEventQueue();
+
+    expect(controller.chapterId, 102);
+    expect(controller.chapterIndex, 2);
+    expect(controller.currentPage, 0);
+    expect(controller.sliderDraft, isNull);
+    expect(controller.currentChapter?.id, 102);
+    expect(dataSource.chapterFetches[102], 1);
+    expect(dataSource.savedProgress, ['9/101', '9/102']);
+  });
+
+  test('章节失败进入错误态，重试后恢复', () async {
+    dataSource.failingChapters.add(101);
+    final controller = createController();
+    await pumpEventQueue();
+    expect(controller.currentChapterError, isA<StateError>());
+    expect(controller.isLoading, isFalse);
+
+    dataSource.failingChapters.remove(101);
+    controller.retryCurrentChapter();
+    expect(controller.isLoading, isTrue);
+    await pumpEventQueue();
+    expect(controller.currentChapter?.id, 101);
+    expect(controller.currentChapterError, isNull);
+    expect(dataSource.chapterFetches[101], 2);
+  });
+
+  test('scramble 同章并发请求复用 Future，读图返回文件字节', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    final values = await Future.wait([controller.scramble(101), controller.scramble(101)]);
+    expect(values, [221081, 221081]);
+    expect(dataSource.scrambleFetches[101], 1);
+    expect(await controller.readPhoto(101, 'x.webp'), [101, 6]);
+  });
+
+  test('scramble 失败会清除 memo，后续请求可重新获取', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    dataSource.failingScrambles.add(101);
+    await expectLater(controller.scramble(101), throwsStateError);
+    dataSource.failingScrambles.remove(101);
+
+    expect(await controller.scramble(101), 221081);
+    expect(dataSource.scrambleFetches[101], 2);
+  });
+
+  test('控制栏切换会通知，dispose 后事件入口全部忽略', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    var notifications = 0;
+    controller.addListener(() => notifications++);
+    controller.toggleControls();
+    expect(controller.controlsVisible, isFalse);
+    expect(notifications, 1);
+
+    controller.dispose();
+    controller.toggleControls();
+    controller.jumpToPage(5);
+    controller.updateFromPositions(_at(5));
+    expect(controller.controlsVisible, isFalse);
+    expect(controller.currentPage, 0);
+    expect(notifications, 1);
+  });
+}

test/features/reader/widgets/reader_page_image_test.dart
@@ -1,0 +1,112 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmcomic/features/reader/page_geometry.dart';
+import 'package:jmcomic/features/reader/widgets/reader_page_image.dart';
+
+final Uint8List _pngBytes = base64Decode(
+  'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJ'
+  'AAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==',
+);
+
+Widget _wrap(Widget child) => MaterialApp(home: Scaffold(body: child));
+
+ReaderPageImage _image({
+  required String imageName,
+  required PageGeometry geometry,
+  required Future<Uint8List> Function(int, String) readPhoto,
+}) => ReaderPageImage(
+  chapterId: 100,
+  imageName: imageName,
+  index: 0,
+  geometry: geometry,
+  viewportWidth: 200,
+  readPhoto: readPhoto,
+  scrambleOf: (_) async => 999999,
+);
+
+void main() {
+  testWidgets('快速命中缓存时直接显示图片并回填真实比例', (tester) async {
+    final geometry = PageGeometry();
+    await tester.pumpWidget(
+      _wrap(_image(imageName: '1.png', geometry: geometry, readPhoto: (_, _) async => _pngBytes)),
+    );
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+
+    expect(find.byType(RawImage), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(geometry.aspectOf(0), closeTo(1.0, 1e-9));
+  });
+
+  testWidgets('慢请求在延迟后才显示进度提示', (tester) async {
+    final completer = Completer<Uint8List>();
+    await tester.pumpWidget(
+      _wrap(
+        _image(
+          imageName: 'slow.png',
+          geometry: PageGeometry(),
+          readPhoto: (_, _) => completer.future,
+        ),
+      ),
+    );
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    await tester.pump(const Duration(milliseconds: 179));
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    await tester.pump(const Duration(milliseconds: 2));
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    completer.complete(_pngBytes);
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+    expect(find.byType(RawImage), findsOneWidget);
+  });
+
+  testWidgets('切换图片会重新计算转圈延迟，避免立即灰闪', (tester) async {
+    final first = Completer<Uint8List>();
+    final second = Completer<Uint8List>();
+    final geometry = PageGeometry();
+
+    await tester.pumpWidget(
+      _wrap(_image(imageName: 'first.png', geometry: geometry, readPhoto: (_, _) => first.future)),
+    );
+    await tester.pump(const Duration(milliseconds: 200));
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    await tester.pumpWidget(
+      _wrap(
+        _image(imageName: 'second.png', geometry: geometry, readPhoto: (_, _) => second.future),
+      ),
+    );
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+
+    first.complete(_pngBytes);
+    second.complete(_pngBytes);
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+  });
+
+  testWidgets('失败态可点击重试并恢复显示', (tester) async {
+    var attempts = 0;
+    Future<Uint8List> readPhoto(int _, String _) async {
+      attempts++;
+      if (attempts == 1) throw StateError('failed once');
+      return _pngBytes;
+    }
+
+    await tester.pumpWidget(
+      _wrap(_image(imageName: 'retry.png', geometry: PageGeometry(), readPhoto: readPhoto)),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text('第 1 页加载失败，点击重试'), findsOneWidget);
+
+    await tester.tap(find.text('第 1 页加载失败，点击重试'));
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+    expect(attempts, 2);
+    expect(find.byType(RawImage), findsOneWidget);
+  });
+}

Eter-Nights/JmBox

test/features/reader/page_geometry_test.dart
@@ -1,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmbox/core/constants/reader_config.dart';
+import 'package:jmbox/features/reader/page_geometry.dart';
+
+void main() {
+  test('未测页面使用默认比例', () {
+    final geometry = PageGeometry();
+    expect(geometry.aspectOf(0), kDefaultPageAspect);
+    expect(geometry.aspectOf(-1), kDefaultPageAspect);
+  });
+
+  test('本页实测值优先于章内估算', () {
+    final geometry = PageGeometry();
+    geometry.record(0, 1000, 1500);
+    geometry.record(1, 1000, 1700);
+    expect(geometry.aspectOf(0), closeTo(1.5, 1e-9));
+    expect(geometry.aspectOf(9), closeTo(1.7, 1e-9));
+  });
+
+  test('非法页码或尺寸不会污染估算', () {
+    final geometry = PageGeometry();
+    geometry.record(-1, 100, 200);
+    geometry.record(0, 0, 200);
+    geometry.record(1, 100, 0);
+    expect(geometry.aspectOf(5), kDefaultPageAspect);
+  });
+
+  test('异常比例被夹在安全上下限', () {
+    final geometry = PageGeometry();
+    geometry.record(0, 1000, 1);
+    geometry.record(1, 1, 100000);
+    expect(geometry.aspectOf(0), kMinPageAspect);
+    expect(geometry.aspectOf(1), kMaxPageAspect);
+  });
+
+  test('同一页面重新测量后，中位数只统计该页最新值', () {
+    final geometry = PageGeometry();
+    geometry.record(0, 1000, 1000);
+    geometry.record(1, 1000, 3000);
+    geometry.record(0, 1000, 2000);
+
+    // 当前有效页面值为 [2.0, 3.0]，上中位数应为 3.0；旧的 1.0 不应残留为额外样本。
+    expect(geometry.aspectOf(99), closeTo(3.0, 1e-9));
+  });
+
+  test('样本超过上限仍保留每页精确实测值', () {
+    final geometry = PageGeometry();
+    for (var i = 0; i < kGeometrySampleLimit + 5; i++) {
+      geometry.record(i, 1000, 1000 + i);
+    }
+    expect(geometry.aspectOf(0), closeTo(1.0, 1e-9));
+    expect(geometry.aspectOf(kGeometrySampleLimit + 4), closeTo(1.02, 1e-9));
+  });
+}

test/features/reader/reader_controller_test.dart
@@ -1,0 +1,236 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmbox/data/models/album.dart';
+import 'package:jmbox/features/reader/reader_controller.dart';
+
+List<Series> _catalog() => const [
+  Series(id: 100, name: '第一话', sort: '1'),
+  Series(id: 101, name: '第二话', sort: '2'),
+  Series(id: 102, name: '第三话', sort: '3'),
+];
+
+class _FakeReaderDataSource implements ReaderDataSource {
+  _FakeReaderDataSource(this.directory, {List<Series>? catalog}) : catalog = catalog ?? _catalog();
+
+  final Directory directory;
+  final List<Series> catalog;
+  final Map<int, int> chapterFetches = {};
+  final Map<int, int> scrambleFetches = {};
+  final List<String> photoReads = [];
+  final List<String> savedProgress = [];
+  final Set<int> failingChapters = {};
+  final Set<int> failingScrambles = {};
+  final Set<String> failPhotoOnce = {};
+
+  @override
+  Future<Chapter> fetchChapter(int chapterId) async {
+    chapterFetches.update(chapterId, (count) => count + 1, ifAbsent: () => 1);
+    if (failingChapters.contains(chapterId)) throw StateError('chapter $chapterId failed');
+    return Chapter(
+      id: chapterId,
+      series: catalog,
+      images: [for (var i = 0; i < 10; i++) '$i.webp'],
+    );
+  }
+
+  @override
+  Future<int> fetchScrambleId(int chapterId) async {
+    scrambleFetches.update(chapterId, (count) => count + 1, ifAbsent: () => 1);
+    if (failingScrambles.contains(chapterId)) {
+      throw StateError('scramble $chapterId failed');
+    }
+    return 220980 + chapterId;
+  }
+
+  @override
+  Future<File> readPhoto(int chapterId, String imageName) async {
+    final key = '$chapterId/$imageName';
+    photoReads.add(key);
+    if (failPhotoOnce.remove(key)) throw StateError('photo failed');
+    return File('${directory.path}/photo.bin');
+  }
+
+  @override
+  Future<void> saveProgress(int albumId, int chapterId) async {
+    savedProgress.add('$albumId/$chapterId');
+  }
+}
+
+Iterable<({int index, double leading, double trailing})> _at(int index) => [
+  (index: index, leading: 0.0, trailing: 1.0),
+];
+
+void main() {
+  late Directory tempDir;
+  late _FakeReaderDataSource dataSource;
+
+  setUp(() async {
+    tempDir = await Directory.systemTemp.createTemp('jmbox_reader_test_');
+    await File('${tempDir.path}/photo.bin').writeAsBytes([101, 6]);
+    dataSource = _FakeReaderDataSource(tempDir);
+  });
+
+  tearDown(() async {
+    if (await tempDir.exists()) await tempDir.delete(recursive: true);
+  });
+
+  ReaderController createController({int initialChapterId = 101, int? albumId = 9}) {
+    final controller = ReaderController(
+      dataSource: dataSource,
+      albumId: albumId,
+      initialChapterId: initialChapterId,
+    );
+    addTearDown(controller.dispose);
+    return controller;
+  }
+
+  test('初始章加载后建立章节列表、预热相邻章并保存进度', () async {
+    final controller = createController();
+    expect(controller.isLoading, isTrue);
+
+    await pumpEventQueue();
+
+    expect(controller.currentChapter?.id, 101);
+    expect(controller.chapterIndex, 1);
+    expect(controller.chapter.name, '第二话');
+    expect(controller.pageCount, 10);
+    expect(dataSource.chapterFetches.keys, containsAll([100, 101, 102]));
+    expect(dataSource.chapterFetches[101], 1);
+    expect(dataSource.savedProgress, ['9/101']);
+  });
+
+  test('albumId 缺失时不保存进度', () async {
+    createController(albumId: null);
+    await pumpEventQueue();
+    expect(dataSource.savedProgress, isEmpty);
+  });
+
+  test('可视面积最大的页面成为当前页并触发有序预取', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    controller.updateFromPositions([
+      (index: 2, leading: -0.8, trailing: 0.2),
+      (index: 3, leading: 0.2, trailing: 0.9),
+    ]);
+    await pumpEventQueue();
+
+    expect(controller.currentPage, 3);
+    expect(dataSource.photoReads, [
+      '101/4.webp',
+      '101/5.webp',
+      '101/6.webp',
+      '101/7.webp',
+      '101/8.webp',
+      '101/2.webp',
+      '101/1.webp',
+    ]);
+  });
+
+  test('重复覆盖同一预取窗口不会重复读取，失败页允许重试', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    dataSource.failPhotoOnce.add('101/4.webp');
+    controller.updateFromPositions(_at(3));
+    await pumpEventQueue();
+    controller.updateFromPositions(_at(5));
+    await pumpEventQueue();
+
+    expect(dataSource.photoReads.where((key) => key == '101/4.webp'), hasLength(2));
+    expect(dataSource.photoReads.where((key) => key == '101/6.webp'), hasLength(1));
+  });
+
+  test('跳页夹取边界，Slider 松手才真正跳页', () async {
+    final scrolled = <int>[];
+    final controller = ReaderController(
+      dataSource: dataSource,
+      albumId: 9,
+      initialChapterId: 101,
+      requestScroll: scrolled.add,
+    );
+    addTearDown(controller.dispose);
+    await pumpEventQueue();
+
+    controller.jumpToPage(99);
+    expect(controller.currentPage, 9);
+    expect(scrolled, [9]);
+    controller.onSliderChanged(3.6);
+    expect(controller.currentPage, 9);
+    expect(controller.sliderDraft, 3.6);
+    controller.onSliderEnd();
+    expect(controller.currentPage, 4);
+    expect(controller.sliderDraft, isNull);
+    expect(scrolled, [9, 4]);
+  });
+
+  test('切章重置页面草稿、复用预热缓存并保存新进度', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    controller.jumpToPage(8);
+    controller.onSliderChanged(4);
+    controller.selectNextChapter();
+    await pumpEventQueue();
+
+    expect(controller.chapterId, 102);
+    expect(controller.chapterIndex, 2);
+    expect(controller.currentPage, 0);
+    expect(controller.sliderDraft, isNull);
+    expect(controller.currentChapter?.id, 102);
+    expect(dataSource.chapterFetches[102], 1);
+    expect(dataSource.savedProgress, ['9/101', '9/102']);
+  });
+
+  test('章节失败进入错误态，重试后恢复', () async {
+    dataSource.failingChapters.add(101);
+    final controller = createController();
+    await pumpEventQueue();
+    expect(controller.currentChapterError, isA<StateError>());
+    expect(controller.isLoading, isFalse);
+
+    dataSource.failingChapters.remove(101);
+    controller.retryCurrentChapter();
+    expect(controller.isLoading, isTrue);
+    await pumpEventQueue();
+    expect(controller.currentChapter?.id, 101);
+    expect(controller.currentChapterError, isNull);
+    expect(dataSource.chapterFetches[101], 2);
+  });
+
+  test('scramble 同章并发请求复用 Future，读图返回文件字节', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    final values = await Future.wait([controller.scramble(101), controller.scramble(101)]);
+    expect(values, [221081, 221081]);
+    expect(dataSource.scrambleFetches[101], 1);
+    expect(await controller.readPhoto(101, 'x.webp'), [101, 6]);
+  });
+
+  test('scramble 失败会清除 memo，后续请求可重新获取', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    dataSource.failingScrambles.add(101);
+    await expectLater(controller.scramble(101), throwsStateError);
+    dataSource.failingScrambles.remove(101);
+
+    expect(await controller.scramble(101), 221081);
+    expect(dataSource.scrambleFetches[101], 2);
+  });
+
+  test('控制栏切换会通知，dispose 后事件入口全部忽略', () async {
+    final controller = createController();
+    await pumpEventQueue();
+    var notifications = 0;
+    controller.addListener(() => notifications++);
+    controller.toggleControls();
+    expect(controller.controlsVisible, isFalse);
+    expect(notifications, 1);
+
+    controller.dispose();
+    controller.toggleControls();
+    controller.jumpToPage(5);
+    controller.updateFromPositions(_at(5));
+    expect(controller.controlsVisible, isFalse);
+    expect(controller.currentPage, 0);
+    expect(notifications, 1);
+  });
+}

test/features/reader/widgets/reader_page_image_test.dart
@@ -1,0 +1,112 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmbox/features/reader/page_geometry.dart';
+import 'package:jmbox/features/reader/widgets/reader_page_image.dart';
+
+final Uint8List _pngBytes = base64Decode(
+  'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJ'
+  'AAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==',
+);
+
+Widget _wrap(Widget child) => MaterialApp(home: Scaffold(body: child));
+
+ReaderPageImage _image({
+  required String imageName,
+  required PageGeometry geometry,
+  required Future<Uint8List> Function(int, String) readPhoto,
+}) => ReaderPageImage(
+  chapterId: 100,
+  imageName: imageName,
+  index: 0,
+  geometry: geometry,
+  viewportWidth: 200,
+  readPhoto: readPhoto,
+  scrambleOf: (_) async => 999999,
+);
+
+void main() {
+  testWidgets('快速命中缓存时直接显示图片并回填真实比例', (tester) async {
+    final geometry = PageGeometry();
+    await tester.pumpWidget(
+      _wrap(_image(imageName: '1.png', geometry: geometry, readPhoto: (_, _) async => _pngBytes)),
+    );
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+
+    expect(find.byType(RawImage), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(geometry.aspectOf(0), closeTo(1.0, 1e-9));
+  });
+
+  testWidgets('慢请求在延迟后才显示进度提示', (tester) async {
+    final completer = Completer<Uint8List>();
+    await tester.pumpWidget(
+      _wrap(
+        _image(
+          imageName: 'slow.png',
+          geometry: PageGeometry(),
+          readPhoto: (_, _) => completer.future,
+        ),
+      ),
+    );
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    await tester.pump(const Duration(milliseconds: 179));
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    await tester.pump(const Duration(milliseconds: 2));
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    completer.complete(_pngBytes);
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+    expect(find.byType(RawImage), findsOneWidget);
+  });
+
+  testWidgets('切换图片会重新计算转圈延迟，避免立即灰闪', (tester) async {
+    final first = Completer<Uint8List>();
+    final second = Completer<Uint8List>();
+    final geometry = PageGeometry();
+
+    await tester.pumpWidget(
+      _wrap(_image(imageName: 'first.png', geometry: geometry, readPhoto: (_, _) => first.future)),
+    );
+    await tester.pump(const Duration(milliseconds: 200));
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    await tester.pumpWidget(
+      _wrap(
+        _image(imageName: 'second.png', geometry: geometry, readPhoto: (_, _) => second.future),
+      ),
+    );
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+
+    first.complete(_pngBytes);
+    second.complete(_pngBytes);
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+  });
+
+  testWidgets('失败态可点击重试并恢复显示', (tester) async {
+    var attempts = 0;
+    Future<Uint8List> readPhoto(int _, String _) async {
+      attempts++;
+      if (attempts == 1) throw StateError('failed once');
+      return _pngBytes;
+    }
+
+    await tester.pumpWidget(
+      _wrap(_image(imageName: 'retry.png', geometry: PageGeometry(), readPhoto: readPhoto)),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text('第 1 页加载失败，点击重试'), findsOneWidget);
+
+    await tester.tap(find.text('第 1 页加载失败，点击重试'));
+    await tester.runAsync(() => Future<void>.delayed(const Duration(milliseconds: 200)));
+    await tester.pump();
+    expect(attempts, 2);
+    expect(find.byType(RawImage), findsOneWidget);
+  });
+}